768368e86bedd859ec32f46c11ab321ad245e9aa
Dương Khiêu <khieudv@Duongs-MacBook-Air.local>
2021-02-19 00:55:49 +0700

    sourcecode for arclinesoft

---
+++ ArclineSoft/arclinesoft-backend/src/main/java/arclinesoft/crudexample/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package arclinesoft.crudexample.repository;
+
+import arclinesoft.crudexample.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
---
+++ ArclineSoft/arclinesoft-backend/src/test/java/arclinesoft/crudexample/Springboot2JpaCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package arclinesoft.crudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2JpaCrudExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
